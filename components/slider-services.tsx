771a1bb9d9e6fea83291dd20f1f57a54254e4ee5
Arturo Pulido <arthurwolf.dev07@gmail.com>
2025-01-06 12:14:37 -0600

    Update images management 1.1

--- components/slider-services.tsx
+++ components/slider-services.tsx
@@ -23,7 +23,7 @@ const SliderServices = () => {
                         {/* Imágenes */}
                         <div className="grid grid-cols-4 items-bottom gap-2 mb-1">
                             {item.img?.map((img, idx) => (
-                                <div key={idx} className="w-full h-12 relative">
+                                <div key={idx} className="relative w-full aspect-square">
                                     {img ? (
                                         <Image
                                             src={img}
@@ -33,11 +33,11 @@ const SliderServices = () => {
                                             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
                                         />
                                     ) : (
-                                        <div className="flex items-center justify-center w-full h-12 bg-gray-200 rounded-lg">
+                                        <div className="flex items-center justify-center w-full h-full bg-gray-200 rounded-lg">
                                             <span className="text-gray-500 text-xs">No image</span>
                                         </div>
                                     )}
-                                </div>
+                                </div>                            
                             ))}
                         </div>
                     </div>
